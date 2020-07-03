1dec7a92e486f3cc12f7128af81125d7db762c5a
Anthonyhelka <helkaanthony1997@gmail.com>
2020-07-03 03:38:12 -0700

    Initial Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform _player;
+    private Transform _playerHead;
+    private float _mouseX, _mouseY, _xRotation;
+    [SerializeField] private float _mouseSensitivity = 50.0f;
+
+    void Start()
+    {
+        _player = GameObject.Find("Player").GetComponent<Transform>();
+        if (_player == null) Debug.LogError("Player is NULL");
+        _playerHead = GameObject.Find("PlayerHead").GetComponent<Transform>();
+        if (_playerHead == null) Debug.LogError("PlayerHead is NULL");
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        GetInput();
+
+        _xRotation -= _mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -60.0f, 60.0f);
+        transform.localRotation = Quaternion.Euler(_xRotation, 0.0f, 0.0f);
+        _playerHead.localRotation = Quaternion.Euler(_xRotation, 0.0f, 0.0f);
+
+        _player.Rotate(0.0f, _mouseX * _mouseSensitivity * Time.deltaTime, 0.0f);
+    }
+
+    void GetInput()
+    {
+        _mouseX = Input.GetAxisRaw("Mouse X");
+        _mouseY = Input.GetAxisRaw("Mouse Y");
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController _characterController;
+    private float _horizontalInput, _verticalInput;
+    private bool _sprintInput, _jumpInput;
+    private Vector3 _direction;
+
+    [Header("Movement Settings")]
+    [SerializeField] private float walkingSpeed = 7.5f;
+    [SerializeField] private float runningSpeed = 11.5f;
+    [SerializeField] private float _jumpHeight = 8.0f;
+    [SerializeField] private float _gravity = 20.0f;
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+        if (_characterController == null) Debug.LogError("CharacterController is NULL");
+    }
+
+    void Update()
+    {
+        GetInput();
+        CalculateMovement();
+    }
+
+    void GetInput()
+    {
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");
+        _sprintInput = Input.GetKey(KeyCode.LeftShift);
+        _jumpInput = Input.GetKeyDown(KeyCode.Space);
+    }
+
+    void CalculateMovement()
+    {
+        float directionY = _direction.y;
+        _direction = new Vector3(_horizontalInput, 0.0f, _verticalInput).normalized;
+
+        _direction.x = (_sprintInput ? runningSpeed : walkingSpeed) * _direction.x;
+        _direction.z = (_sprintInput ? runningSpeed : walkingSpeed) * _direction.z;
+
+        if (_jumpInput && _characterController.isGrounded)
+        {
+            _direction.y = _jumpHeight;
+        }
+        else
+        {
+            _direction.y = directionY;
+        }
+
+        if (!_characterController.isGrounded)
+        {
+            _direction.y -= _gravity * Time.deltaTime;
+        }
+
+        _direction = transform.TransformDirection(_direction);
+        _characterController.Move(_direction * Time.deltaTime);
+    }
+}
