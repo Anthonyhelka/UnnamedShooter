30711ea754874b4813be00b9e2d8daa15678a9f2
Anthony Helka <helkaanthony1997@gmail.com>
2020-07-05 02:47:55 -0400

    Added gun mechanics.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     private Transform _player;
-    private Transform _playerHead;
     private float _mouseX, _mouseY, _xRotation;
     [SerializeField] private float _mouseSensitivity = 50.0f;
 
@@ -13,8 +12,6 @@ public class CameraController : MonoBehaviour
     {
         _player = GameObject.Find("Player").GetComponent<Transform>();
         if (_player == null) Debug.LogError("Player is NULL");
-        _playerHead = GameObject.Find("PlayerHead").GetComponent<Transform>();
-        if (_playerHead == null) Debug.LogError("PlayerHead is NULL");
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -25,7 +22,6 @@ public class CameraController : MonoBehaviour
         _xRotation -= _mouseY;
         _xRotation = Mathf.Clamp(_xRotation, -60.0f, 60.0f);
         transform.localRotation = Quaternion.Euler(_xRotation, 0.0f, 0.0f);
-        _playerHead.localRotation = Quaternion.Euler(_xRotation, 0.0f, 0.0f);
 
         _player.Rotate(0.0f, _mouseX * _mouseSensitivity * Time.deltaTime, 0.0f);
     }
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10.0f;
+    public float range = 100.0f;
+    public Camera camera;
+    public ParticleSystem muzzleFlash;
+    public float fireRate = 0.15f;
+    public float canFire = -0.1f;
+    public float recoilRotation;
+    public Transform container;
+    public float minRecoil = 0.4f;
+    public float maxRecoil = 0.6f;
+    public float recoil;
+    public Animator animator;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (canFire < Time.time)
+            {
+                Shoot();
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        if(Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+        Recoil();
+        canFire = Time.time + fireRate;
+    }
+
+    void Recoil()
+    {
+        animator.SetTrigger("Firing");
+        recoilRotation += Random.Range(minRecoil, maxRecoil);
+        container.transform.localRotation = Quaternion.Euler(-recoilRotation, 0.0f, 0.0f);
+    }
+}
