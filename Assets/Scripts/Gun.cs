6577d7d3f34cca8edc109c1a0eb78fb9d3f4887f
Anthonyhelka <helkaanthony1997@gmail.com>
2020-07-07 02:15:49 -0700

    Added weapon animations in scripts.

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,58 +4,130 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    public float damage = 10.0f;
-    public float range = 100.0f;
-    public Camera camera;
-    public ParticleSystem muzzleFlash;
-    public float fireRate = 0.15f;
-    public float canFire = -0.1f;
-    public float recoilRotation;
-    public Transform container;
-    public float minRecoil = 0.4f;
-    public float maxRecoil = 0.6f;
-    public float recoil;
-    public Animator animator;
-    public AudioSource audioSource;
+    [Header("General Settings")]
+    [SerializeField] private float _range = 100.0f;
+    [SerializeField] private float _minRecoil = 0.4f;
+    [SerializeField] private float _maxRecoil = 0.6f;
+    [SerializeField] private float _impactForce = 100.0f;
+    [SerializeField] private float _visualRecoilAngle = 2.5f;
+    [SerializeField] private float _sprintSlerpSpeed = 5.0f;
 
-    void Start()
+    [Header("Primary Fire Settings")]
+    [SerializeField] private float _primaryDamage = 10.0f;
+    [SerializeField] private float _primaryFireRate = 0.3f;
+
+    [Header("Secondary Fire Settings")]
+    [SerializeField] private float _secondaryDamage = 8.0f;
+    [SerializeField] private float _secondaryFireRate = 0.75f;
+    [SerializeField] private float _burstFireRate = 0.15f;
+    [SerializeField] private float _burstFireAmount = 3;
+
+    private Camera _playerCamera;
+    private ParticleSystem _muzzleFlash;
+    private Transform _cameraContainer;
+    private Animator _animator;
+    private AudioSource _audioSource;
+
+    private bool _canFire = true;
+    private float _fireCooldown = -0.1f;
+    private float _recoil = 0.0f;
+    private Vector3 _sprintSmoothDampVelocity, _recoilSmoothDampVelocity =  Vector3.zero;
+    private float _totalVisualRecoilAngle;
+    private float _recoilAngleSmoothDampVelocity = 0.0f;
+    private Quaternion _targetSprintRotation = Quaternion.Euler(new Vector3(2.1f, -53.2f, 34.0f));
+    private float _totalSprintSlerp;
+
+    void  Start()
     {
-        
+        _playerCamera = GameObject.Find("PlayerCamera").GetComponent<Camera>();
+        if (_playerCamera == null) Debug.LogError("PlayerCamera is NULL");
+        _muzzleFlash = GameObject.Find("MuzzleFlash").GetComponent<ParticleSystem>();
+        if (_muzzleFlash == null) Debug.LogError("MuzzleFlash is NULL");
+        _cameraContainer = GameObject.Find("CameraContainer").GetComponent<Transform>();
+        if (_cameraContainer == null) Debug.LogError("CameraContainer is NULL");
+        _animator = GetComponent<Animator>();
+        if (_animator == null) Debug.LogError("Animator is NULL");
+        _audioSource = GetComponent<AudioSource>();
+        if (_audioSource == null) Debug.LogError("AudioSource is NULL");
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
+        transform.localPosition = Vector3.SmoothDamp(transform.localPosition, new Vector3(0.125f, -0.225f, 0.45f), ref _recoilSmoothDampVelocity, 0.1f);
+        _totalVisualRecoilAngle = Mathf.SmoothDamp(_totalVisualRecoilAngle, 0, ref _recoilAngleSmoothDampVelocity, 0.1f);
+        transform.localEulerAngles = transform.localEulerAngles = Vector3.left * _totalVisualRecoilAngle;
+    }
+
+    public void PrimaryFire()
+    {
+        if (_fireCooldown < Time.time && _canFire)
         {
-            if (canFire < Time.time)
-            {
-                Shoot();
-            }
+            FireCoroutine(_primaryDamage, _primaryFireRate);
+            _fireCooldown = Time.time + _primaryFireRate;
         }
     }
 
-    void Shoot()
+    public void SecondaryFire()
     {
-        muzzleFlash.Play();
-        audioSource.Play();
-        RaycastHit hit;
-        if(Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range))
+        if (_fireCooldown < Time.time && _canFire)
         {
-            Debug.Log(hit.transform.name);
+            StartCoroutine(BurstCoroutine());
+            _fireCooldown = Time.time + _secondaryFireRate;
+        }
+    }
+
+    private void FireCoroutine(float damage, float fireRate)
+    {
+        _muzzleFlash.Play();
+        _audioSource.Play();
+        Recoil();
+        RaycastHit hit;
+        if(Physics.Raycast(_playerCamera.transform.position,_playerCamera.transform.forward, out hit, _range))
+        {
             Target target = hit.transform.GetComponent<Target>();
             if (target != null)
             {
                 target.TakeDamage(damage);
             }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * _impactForce);
+            }
         }
-        Recoil();
-        canFire = Time.time + fireRate;
     }
 
-    void Recoil()
+    private void Recoil()
     {
-        animator.SetTrigger("Firing");
-        recoilRotation += Random.Range(minRecoil, maxRecoil);
-        container.transform.localRotation = Quaternion.Euler(-recoilRotation, 0.0f, 0.0f);
+        _recoil += Random.Range(_minRecoil, _maxRecoil);
+        _cameraContainer.transform.localRotation = Quaternion.Euler(-_recoil, 0.0f, 0.0f);
+        transform.localPosition -= Vector3.forward * 0.1f;
+        _totalVisualRecoilAngle += _visualRecoilAngle;
+        _totalVisualRecoilAngle = Mathf.Clamp(_totalVisualRecoilAngle, 0, 30);
+    }
+
+    private IEnumerator BurstCoroutine()
+    {   
+        for (int i = 0; i < _burstFireAmount; i++) 
+            {
+                FireCoroutine(_secondaryDamage, _burstFireRate);
+                yield return new WaitForSeconds(_burstFireRate);
+            }
+    }
+
+    public void Sprint(bool isSprinting)
+    {   
+        transform.localPosition = Vector3.SmoothDamp(transform.localPosition, new Vector3(0.07f, -0.27f, 0.44f), ref _sprintSmoothDampVelocity, 2.0f);
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, _targetSprintRotation, _totalSprintSlerp);
+        if (isSprinting)
+        {   
+            _totalSprintSlerp += _sprintSlerpSpeed * Time.deltaTime;
+        }
+        else if(!isSprinting)    
+        {   
+            _totalSprintSlerp -= _sprintSlerpSpeed * Time.deltaTime;
+        }
+        _totalSprintSlerp = Mathf.Clamp(_totalSprintSlerp, 0, 1);
+        _canFire = _totalSprintSlerp == 0 ? true : false;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private CharacterController _characterController;
     private float _horizontalInput, _verticalInput;
-    private bool _sprintInput, _jumpInput;
+    private bool _sprintInput, _jumpInput, _primaryFireInput, _secondaryFireInput;
     private Vector3 _direction;
 
     [Header("Movement Settings")]
@@ -14,17 +14,23 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float runningSpeed = 11.5f;
     [SerializeField] private float _jumpHeight = 8.0f;
     [SerializeField] private float _gravity = 20.0f;
+    
+    private Gun _gun;
+    public Animator _playerAnimator;
 
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
         if (_characterController == null) Debug.LogError("CharacterController is NULL");
+        _gun = GameObject.Find("Gun").GetComponent<Gun>();
+        if (_gun == null) Debug.LogError("Gun is NULL");
     }
 
     void Update()
     {
         GetInput();
         CalculateMovement();
+        FireWeapon();
     }
 
     void GetInput()
@@ -33,6 +39,8 @@ public class PlayerController : MonoBehaviour
         _verticalInput = Input.GetAxisRaw("Vertical");
         _sprintInput = Input.GetKey(KeyCode.LeftShift);
         _jumpInput = Input.GetKeyDown(KeyCode.Space);
+        _primaryFireInput = Input.GetButton("Fire1");
+        _secondaryFireInput = Input.GetButton("Fire2");
     }
 
     void CalculateMovement()
@@ -40,8 +48,27 @@ public class PlayerController : MonoBehaviour
         float directionY = _direction.y;
         _direction = new Vector3(_horizontalInput, 0.0f, _verticalInput).normalized;
 
-        _direction.x = (_sprintInput ? runningSpeed : walkingSpeed) * _direction.x;
-        _direction.z = (_sprintInput ? runningSpeed : walkingSpeed) * _direction.z;
+        if (_sprintInput && _verticalInput > 0.0f)
+        {
+            _direction.x = runningSpeed * _direction.x;
+            _direction.z = runningSpeed * _direction.z;
+            _gun.Sprint(true);
+            if (_characterController.isGrounded)
+            {
+                _playerAnimator.SetBool("isRunning", true);
+            }
+            else
+            {
+                _playerAnimator.SetBool("isRunning", false);
+            }
+        }
+        else
+        {
+            _direction.x = walkingSpeed * _direction.x;
+            _direction.z = walkingSpeed * _direction.z;
+            _playerAnimator.SetBool("isRunning", false);
+            _gun.Sprint(false);
+        }
 
         if (_jumpInput && _characterController.isGrounded)
         {
@@ -59,5 +86,18 @@ public class PlayerController : MonoBehaviour
 
         _direction = transform.TransformDirection(_direction);
         _characterController.Move(_direction * Time.deltaTime);
+    }
+
+    void FireWeapon()
+    {
+       if (_primaryFireInput)
+        {
+            _gun.PrimaryFire();
+        }
+
+        if (_secondaryFireInput)
+        {
+            _gun.SecondaryFire();
+        }
     }
 }
